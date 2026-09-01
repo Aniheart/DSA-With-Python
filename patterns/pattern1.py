28633cfb94cb2ff04e84ab0fac9629ff683dcf4f
Aniket <aniketk65310@gmail.com>
2026-09-01 19:14:39 +0530

    pattern1

---
+++ patterns/pattern1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def pattern1(self, n):
+        for i in range(0,n,1):
+            for j in range(0,n,1):
+                print("*", end="")
+            print(end="\n")
+
+sol = Solution()
+sol.pattern1(1)
